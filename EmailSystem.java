65135aeaf588966d78191934c95e6778dcea3b2c
Andrea Vasquez <amv445@nyu.edu>
2018-09-22 02:15:31 -0400

    added HashMap

--- EmailSystem.java
+++ EmailSystem.java
@@ -1,21 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-public class CSVReader{
+public class EmailSystem{
 
     public static void main(String[] args){
-        String csvFile = "";
-        String line = "";
+        String csvFile = "/home/andrea/CodeForGood/team-9"; // name of csv
+        String line = ""; //each line being read
+        Map<String,String> cohorts = new HashMap<>();
 
         try(BufferedReader br = new BufferedReader(new FileReader(csvFile))){
-        
-            while((line = br.readLine()) != null){
-                String [] donor = line.split(",");
-                int output = donor[9];
-
+            String first_line = br.readLine();
+            while((line = br.readLine()) != null){ // reads each line
+                String [] donor = line.split(","); //puts it into an array
+                String zip = donor[11]; // zipcode location
+                String email = donor[3];
                 
+                cohorts.put(zip, email);
+    
             }
         }
+        System.out.println(cohorts);
     }
 
 }
