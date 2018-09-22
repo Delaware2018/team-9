278d22ab1620f1d93b618340e3e9c4c845db6aa2
Andrea Vasquez <amv445@nyu.edu>
2018-09-22 03:11:40 -0400

    Implemended an ArrayList for multiple emails in zip

--- EmailSystem.java
+++ EmailSystem.java
@@ -9,18 +9,27 @@ public class EmailSystem{
     public static void main(String[] args){
         String csvFile = "/home/andrea/CodeForGood/Sample_data.csv"; // name of csv
         String line = ""; //each line being read
-        Map<String,String> cohorts = new HashMap<>();
+        Map<String,ArrayList<String>> cohorts = new HashMap<>();
 
         try(BufferedReader br = new BufferedReader(new FileReader(csvFile))){
             String first_line = br.readLine();
+            
             while((line = br.readLine()) != null){ // reads each line
                 String [] donor = line.split(","); //puts it into an array
                 String zip = donor[11]; // zipcode location
                 String email = donor[3];
+                ArrayList<String> emailList = new ArrayList<String>();
                 
-                cohorts.put(zip, email);
+                if (cohorts.containsKey(zip)){
+                    ArrayList<String> copyEmails = cohorts.get(zip);
+                    copyEmails.add(email);
+                    cohorts.put(zip, copyEmails);
+                }
+                else{ 
+                emailList.add(email);
+                cohorts.put(zip, emailList);
                 System.out.println(zip);
-    
+                }
             }
         }
         catch (IOException e) {
